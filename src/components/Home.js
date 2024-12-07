bd0ac6e607c5ba2e2c1e1da2bc75e15beb401ba4
Sarah <55793495+sarahdeve@users.noreply.github.com>
2024-12-07 22:16:25 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
           <div className='row home-page-body'>
           <h1>You leave it we wash it!</h1>
           <p>Your one-stop solution for all your laundry needs.</p>
-          <p>Laundry Service is designed to be the easiest way to get your laundry or dry-cleaning done, so you can go back to doing what you love.</p>
+          <p>Sena Laundry is designed to be the easiest way to get your laundry or dry-cleaning done, so you can go back to doing what you love.</p>
          <small>Save Time, Enjoy Life.</small>
           </div>
        </div>
